9b297a05a3389d0ddfb942abb4036e2f78c9b428
Amy Spears <amspears007@gmail.com>
2023-01-30 20:46:56 -0700

    Add passing card in category set

--- lib/deck.rb
+++ lib/deck.rb
@@ -8,4 +8,21 @@ class Deck
   def cards
     @cards
   end
+
+  def count
+    cards.count
+  end
+
+  def cards_in_category(category)
+    #using each
+    card_category = []
+    cards.each do |card|
+      card.category == category
+      card_category << card if card.category == category
+    end
+    card_category
+    #REFACTOR using select
+    #cards.select do |card|
+    #card.category == category
+  end
 end
