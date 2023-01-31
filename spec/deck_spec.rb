8e5c18ed687df4e008ca3f752c644e85edb9547a
Amy Spears <amspears007@gmail.com>
2023-01-30 20:19:27 -0700

    Add Deck class and test file with exists test

---
+++ spec/deck_spec.rb
@@ -0,0 +1,18 @@
+require './lib/deck'
+# require'./lib/turn'
+require './lib/card'
+
+RSpec.describe Deck do
+  it 'exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+
+    cards = [card_1, card_2, card_3]
+
+    deck = Deck.new(cards)
+
+    expect(deck).to be_instance_of(Deck)
+    expect(deck.cards).to match_array([card_1, card_2, card_3])\
+  end
+end
